89199729929d884966dde5d6c2516f1c3fd375bd
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-01-24 15:28:59 +0100

    rendu 2.1

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,1 +1,8 @@
+<?php
+include '../ressources/views/layouts/head.php';
+include '../ressources/views/layouts/header.php';
+?>
 <h1>BOUH</h1>
+<?php
+include '../ressources/views/layouts/footer.php';
+?>
--- public/index.php
+++ public/index.php
@@ -1,16 +1,17 @@
 <?php
 
-echo'coucou'.'</br>';
-include '../config/database.php';
+// echo'coucou'.'</br>';
+// include '../config/database.php';
+
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
+$action = $action ?? 'Accueil';
 
 switch ($action) {
+    case 'Accueil':
+        include '../app/controllers/homeController.php';
+        break;
     default:
-    include '../app/controllers/homeController.php';
-        // header('HTTP/1.0 404 Not Found');
-        // $metatitle = 'Erreur 404 - Not Found';
-        // include '../ressources/views/layouts/head.php';
-        // include '../ressources/views/layouts/header.php';
-        // include '../ressources/views/errors/404.php';
-        // include '../ressources/views/layouts/footer.php';
+        header('HTTP/1.0 404 Not Found');
+        $metatitle = 'Erreur 404 - Not Found';
+        include '../ressources/views/errors/404.php';
 }
