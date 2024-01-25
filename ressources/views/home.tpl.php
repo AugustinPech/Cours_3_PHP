b3c75ae497985e5967519157c604de50e59f946f
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-01-25 14:09:58 +0100

    rendu 2.4

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -4,21 +4,24 @@ include '../ressources/views/layouts/head.tpl.php';
 include '../ressources/views/layouts/header.tpl.php';
 ?>
 <div class="d-flex col-12 flex-row flex-wrap justify-content-around ">
-    <?php if (count($lastArticles)>0) : foreach ($lastArticles as $article) : ?>
-        <div class="d-flex flex-column col-4 m-1">
-            <h3 class="border-bottom" >
-                <?php echo $article['title'] ?>
-            </h3>
-            <blockquote cite="">
-                <p>
-                    <?php echo $article['body'] ?>
-                </p>
-                <footer>
-                <em>By <u><?php echo $article['pseudo'] ?></u> </em>
-                </footer>
-            </blockquote>
-        </div>
-    <?php endforeach; else : ?>
+    <?php if (count($lastArticles) > 0) : foreach ($lastArticles as $article) : ?>
+            <div class="d-flex flex-column col-4 m-1">
+                <a href="" class="link-dark">
+                    <h3 class="border-bottom">
+                        <?php echo $article['title'] ?>
+                    </h3>
+                </a>
+                <blockquote cite="">
+                    <p>
+                        <?php echo $article['body'] ?>
+                    </p>
+                    <footer class="m-2">
+                        <em>By <a href="" class=""><u><?php echo $article['pseudo'] ?></u></a> </em>
+                    </footer>
+                </blockquote>
+            </div>
+        <?php endforeach;
+    else : ?>
         <div>
             Aucun articles pour le moment.
         </div>
